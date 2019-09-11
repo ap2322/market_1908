441e1f329fba61b7553ad4663c85c666066773b3
Alice Post <alicep2322@gmail.com>
2019-09-11 14:32:06 -0600

    Add sell method and tests

--- test/market_test.rb
+++ test/market_test.rb
@@ -84,4 +84,19 @@ class MarketTest < Minitest::Test
     assert_equal expected_sorted_items, @market.sorted_item_list
   end
 
+  def test_sell
+    @market.add_vendor(@vendor_1)
+    @market.add_vendor(@vendor_2)
+    @market.add_vendor(@vendor_3)
+
+    assert_equal false, @market.sell("Peaches", 200)
+    assert_equal false, @market.sell("Onions", 1)
+    assert_equal true, @market.sell("Banana Nice Cream", 5)
+
+    assert_equal 45, @vendor_2.check_stock("Banana Nice Cream")
+    assert_equal true, @market.sell("Peaches", 40)
+    assert_equal 0, @vendor_1.check_stock("Peaches")
+    assert_equal 60, @vendor_3.check_stock("Peaches")
+  end
+
 end
